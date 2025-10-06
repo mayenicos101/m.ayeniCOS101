9e33ec4c612839f788e0bc3c3a461fd30844a5c4
Mosopefoluwa Ayeni <mosopefoluwa.ayeni@pau.edu.ng>
2025-10-06 11:49:53 +0100

    (Added an extra line in week-2 practice 1 and uploaded week-2 practice 2)

--- week-2/practice_1.rs
+++ week-2/practice_1.rs
@@ -1,4 +1,5 @@
 fn main(){
+	
 	println!("Welcome to COS 101");
 
 	println!("The course learning outcome is as follows");
---
+++ week-2/practice_2.rs
@@ -0,0 +1,10 @@
+fn main(){
+
+	println!(); //prints nothing tbh
+
+	println!("hello"); //prints hello
+
+	println!("format {} arguments","some"); //prints format some arguments
+
+	println!("My name is {}. I a student of computer science year 1", "Mosopefoluwa Ayeni"); //prints My name is Mosopefoluwa Ayeni. I am a student of computer science year 1
+}
