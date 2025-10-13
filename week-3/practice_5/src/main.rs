6b91a95504dda6be0aff9e80b17ecdec1309a7be
Mosopefoluwa Ayeni <mosopefoluwa.ayeni@pau.edu.ng>
2025-10-13 15:52:31 +0100

    (week 3 practice 5)

---
+++ week-3/practice_5/src/main.rs
@@ -0,0 +1,12 @@
+fn main() {
+    let special_character = '@'; //default
+    let alphabet:char = 'A';
+    let surname = "Ayeni";
+    let first_name = "Mosopefoluwa";
+    let middle_name = "Joseph";
+
+    println!(" ");
+    println!("Special character: {}", special_character);
+    println!("Alphabet: {}", alphabet);
+    println!("Name: {} {} {}",surname, first_name, middle_name);
+}
