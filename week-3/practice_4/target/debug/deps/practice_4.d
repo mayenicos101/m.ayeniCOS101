561742e31abb3cdcfd17a96af6ce0d1697034b3d
Mosopefoluwa Ayeni <mosopefoluwa.ayeni@pau.edu.ng>
2025-10-13 15:44:16 +0100

    (week 3 practice 4)

---
+++ week-3/practice_4/target/debug/deps/practice_4.d
@@ -0,0 +1,5 @@
+C:\Users\its-p\OneDrive\Documents\m.ayeniCOS101\week-3\practice_4\target\debug\deps\practice_4.d: src\main.rs
+
+C:\Users\its-p\OneDrive\Documents\m.ayeniCOS101\week-3\practice_4\target\debug\deps\practice_4.exe: src\main.rs
+
+src\main.rs:
