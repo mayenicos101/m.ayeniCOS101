9bcad01a7a85869bb36792a2a6a62e4beca782d9
Mosopefoluwa Ayeni <mosopefoluwa.ayeni@pau.edu.ng>
2025-10-20 23:44:35 +0100

    (week 4 practice 8)

--- week-4/practice_8/src/main.rs
+++ week-4/practice_8/src/main.rs
@@ -1,11 +1,13 @@
 fn main() {
-    let mut x = 0;
-    loop {
-        x+=1;
-        println!("x={}", x);
+    let mut count = 0;
 
-        if x==15 {
-            break;
+    for num in 1..21 {
+        if num > 10{
+            println!("{}", num);
+            continue;
         }
+        count+=1;
+
     }
+    println!("The count of values greater than 10 (between 1 and 20) is: {}", count);
 }
