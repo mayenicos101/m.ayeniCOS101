444b75bc716a0e1be55e5afd8c1f10c22cd5fb43
Mosopefoluwa Ayeni <mosopefoluwa.ayeni@pau.edu.ng>
2025-11-04 19:19:24 +0100

    (week 6 practice 4)

---
+++ week-6/practice_4/target/debug/deps/practice_4-21a9977480e449fa.d
@@ -0,0 +1,5 @@
+C:\Users\its-p\OneDrive\Documents\m.ayeniCOS101\week-6\practice_4\target\debug\deps\practice_4-21a9977480e449fa.d: src\main.rs
+
+C:\Users\its-p\OneDrive\Documents\m.ayeniCOS101\week-6\practice_4\target\debug\deps\libpractice_4-21a9977480e449fa.rmeta: src\main.rs
+
+src\main.rs:
---
+++ week-6/practice_4/target/debug/practice_4.d
@@ -0,0 +1,1 @@
+C:\Users\its-p\OneDrive\Documents\m.ayeniCOS101\week-6\practice_4\target\debug\practice_4.exe: C:\Users\its-p\OneDrive\Documents\m.ayeniCOS101\week-6\practice_4\src\main.rs
