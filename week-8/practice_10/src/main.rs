bd984146af1858cf8d7dea14d80af39390357f2f
Mosopefoluwa Ayeni <mosopefoluwa.ayeni@pau.edu.ng>
2025-11-22 15:14:23 +0100

    (week 8 practice 10)

---
+++ week-8/practice_10/src/main.rs
@@ -0,0 +1,10 @@
+fn print(x:(i32,bool,f64)) {
+    println!("Inside print method");
+    let (age,is_male,cgpa) = x;
+    println!("Age is {}, Is male? {}, CGPA is {}", age, is_male, cgpa);
+}
+
+fn main() {
+    let b:(i32,bool,f64) = (30, true, 4.9);
+    print(b);
+}
